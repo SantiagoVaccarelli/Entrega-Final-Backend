3cd0b61ce8f5e7e70a322a582d0fb5aa09239926
Santiago Vaccarelli <svaccarelli@fi.uba.ar>
2022-10-06 14:58:19 -0300

    make it better

---
+++ config/configMongoDB.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const config = {
+    mongoDB:{
+        URL:process.env.MONGOURL,
+        options:{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    }
+}
+
+export const connectMongoDB = async () => {
+  try {
+    await mongoose.connect(config.mongoDB.URL, config.mongoDB.options);
+    console.log("Connected Mongo DB");
+  } catch (error) {
+    console.log("Error en la conexión a mongoDB", error);
+  }
+};
